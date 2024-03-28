3b8fab8783ae940c42af3fd81f905c437a82aa86
ZhenyaKasatkina <knopisha.zh@gmail.com>
2024-03-28 20:27:19 +0300

    add  catalog/migrations with 3 migration, change catalog/models.py, config/settings.py, config/urls.py, pyproject.toml

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangohw',
+        'USER': 'postgres'
     }
 }
 
@@ -129,3 +130,6 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
